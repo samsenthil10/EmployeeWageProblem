021d38b7bdddb7a83f407b04778a6ddaef790013
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 02:37:02 +0530

    REFACTORED: To convert if-else concept to switch-case concept

--- src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputationProgram.java
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputationProgram.java
@@ -3,7 +3,7 @@ package com.bridgelabz.employeewagecomputationproblem;
 public class EmployeeWageComputationProgram {
 
 	public static void main(String[] args) {
-
+		final int EMPLOYEE_ABSENT = 0;
 		final int EMPLOYEE_PRESENT_FULL_TIME = 1;
 		final int EMPLOYEE_PRESENT_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
@@ -12,20 +12,17 @@ public class EmployeeWageComputationProgram {
 		final int ABSENT = 0;
 
 		int totalWage = 0;
-		int totalHours = 0;
 
 		double employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-		if(employeeCheck == EMPLOYEE_PRESENT_FULL_TIME) {
-			totalHours = FULL_TIME;
+		switch((int)employeeCheck) {
+		case EMPLOYEE_PRESENT_FULL_TIME: totalWage = FULL_TIME * WAGE_PER_HOUR;
+		break;
+		case EMPLOYEE_PRESENT_PART_TIME: totalWage = PART_TIME * WAGE_PER_HOUR;
+		break;
+		case EMPLOYEE_ABSENT: totalWage = ABSENT * WAGE_PER_HOUR;
+		break;
 		}
-		else if (employeeCheck == EMPLOYEE_PRESENT_PART_TIME) {
-			totalHours = PART_TIME;
-		}
-		else {
-			totalHours = ABSENT;
-		}
-		totalWage= totalHours * WAGE_PER_HOUR;
 		System.out.println("Employee Wage: "+totalWage);
 	}
 }
