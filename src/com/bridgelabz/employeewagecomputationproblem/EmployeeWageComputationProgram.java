83c7fc80b9961f82861f32af7e021f21e53bdd0c
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 02:05:33 +0530

    ADDED: To display Welcome to Employee Wage Computation Program

---
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputationProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewagecomputationproblem;
+
+public class EmployeeWageComputationProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("----------Welcome To Employee Wage Computation Program----------");
+	}
+}
