cfb42d65e9a7afa85a49ea2d36c3809eda671bb8
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-24 02:29:30 +0530

    ADDED: To support part time in daily employee wage calculator

--- src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputationProgram.java
+++ src/com/bridgelabz/employeewagecomputationproblem/EmployeeWageComputationProgram.java
@@ -4,21 +4,26 @@ public class EmployeeWageComputationProgram {
 
 	public static void main(String[] args) {
 
-		final int EMPLOYEE_PRESENT = 1;
+		final int EMPLOYEE_PRESENT_FULL_TIME = 1;
+		final int EMPLOYEE_PRESENT_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int FULL_TIME = 8;
+		final int PART_TIME = 4;
 		final int ABSENT = 0;
 
-		int totalWage =0;
-		int totalHours=0;
+		int totalWage = 0;
+		int totalHours = 0;
 
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+		double employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-		if(employeeCheck == EMPLOYEE_PRESENT) {
-			totalHours= FULL_TIME;
+		if(employeeCheck == EMPLOYEE_PRESENT_FULL_TIME) {
+			totalHours = FULL_TIME;
+		}
+		else if (employeeCheck == EMPLOYEE_PRESENT_PART_TIME) {
+			totalHours = PART_TIME;
 		}
 		else {
-			totalHours= ABSENT;
+			totalHours = ABSENT;
 		}
 		totalWage= totalHours * WAGE_PER_HOUR;
 		System.out.println("Employee Wage: "+totalWage);
